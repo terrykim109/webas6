45d8c424ed4464838cbbed322a387afafb64f8a2
Hyun Joon Kim <hjkim74@myseneca.ca>
2025-08-13 09:02:30 -0400

    another try

--- user-api/server.js
+++ user-api/server.js
@@ -42,7 +42,7 @@ app.use(passport.initialize());
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 // app.set('views', __dirname + '/views'); 
 
 app.get("/", (req, res) => {
